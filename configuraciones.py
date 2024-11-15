b70dfe083c341d366ae8f0e440192e2e4b2c7c2f
EduinRodri <eduin.rodri.7@gmail.com>
2024-11-15 10:46:42 -0500

    menu login

--- configuraciones.py
+++ configuraciones.py
@@ -1,3 +1,23 @@
+usuario = []
+contraseña = []
+acceso = ['Iniciar sesión', 'Registrarse']
+
+print("Bienvenido a nuestro Asistente de Laboratorio")
+def menuUsuario():
+    print("\nAcceso:")
+    for i, opcion_Acceso in enumerate(acceso):
+        print(f"{i+1}: {acceso}")
+
+    acceso_Seleccionado = int(input())
+    if acceso_Seleccionado > 0 and acceso_Seleccionado <= len(acceso):
+        opcion_Acceso = acceso[acceso_Seleccionado-1]
+
+
+
+
+
+
+
 from datetime import datetime
 # constantes
 SELECT = "Seleccione una opcion: "
